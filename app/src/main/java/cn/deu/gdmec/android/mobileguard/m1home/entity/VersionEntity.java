e701d87f8701fb29c9a3ed282de58c6b4be9402b
linyanbin <1215065018@qq.com>
2017-09-27 18:48:41 +0800

    Initial commit

---
+++ app/src/main/java/cn/deu/gdmec/android/mobileguard/m1home/entity/VersionEntity.java
@@ -0,0 +1,11 @@
+package cn.deu.gdmec.android.mobileguard.m1home.entity;
+
+/**
+ * Created by LYB on 2017/9/26.
+ */
+
+public class VersionEntity {
+    public String versioncode;
+    public  String description;
+    public String apkurl;
+}
